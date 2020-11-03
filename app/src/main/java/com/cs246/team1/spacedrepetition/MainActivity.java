64d9d96d99966e6a442a9b7baa25ea47f3e4c0c6
tom metge <tom@accident-prone.com>
2020-11-02 16:12:35 -0800

    Add Firebase auth and LoginActivity

--- app/src/main/java/com/cs246/team1/spacedrepetition/MainActivity.java
+++ app/src/main/java/com/cs246/team1/spacedrepetition/MainActivity.java
@@ -2,13 +2,42 @@ package com.cs246.team1.spacedrepetition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            startActivity(new Intent(this, LoginActivity.class));
+        } else {
+            TextView text = (TextView) findViewById(R.id.hello_world);
+            text.setText("Hello, " + user.getDisplayName());
+        }
+    }
+
+    public void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(Task<Void> task) {
+                        // ...
+                    }
+                });
     }
 }
