84a60fd7f7f7c5849749301610de3f1462937f9d
tom metge <tom@accident-prone.com>
2020-11-03 19:47:21 -0800

    Fix Android Studio's mess up

--- app/src/main/java/com/cs246/team1/spacedrepetition/ReminderController.java
+++ app/src/main/java/com/cs246/team1/spacedrepetition/ReminderController.java
@@ -1,7 +1,7 @@
 package com.cs246.team1.spacedrepetition;
 
 public class ReminderController {
-    public void addReminder(String summary, String content, Int reminderInterval) {
+    public void addReminder(String summary, String content, Integer reminderInterval) {
 
     }
 }
