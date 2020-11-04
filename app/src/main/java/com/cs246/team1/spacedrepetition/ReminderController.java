879a83ad286dd12b40e67e3f280eed6f2c4bf688
Adrian Ruiz <adrianruizb18@gmail.com>
2020-11-03 22:43:43 -0500

    Added ReminderController and ReminderUnitTest

---
+++ app/src/main/java/com/cs246/team1/spacedrepetition/ReminderController.java
@@ -0,0 +1,7 @@
+package com.cs246.team1.spacedrepetition;
+
+public class ReminderController {
+    public void addReminder(String summary, String content, Int reminderInterval) {
+
+    }
+}
---
+++ app/src/test/java/com/cs246/team1/spacedrepetition/ReminderUnitTest.java
@@ -0,0 +1,18 @@
+package com.cs246.team1.spacedrepetition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ReminderTest {
+    @Test
+    public void addReminder_isCorrect() {
+        ReminderController testReminder = new ReminderController();
+        testReminder.addReminder("test summary", "test content", 1);
+    }
+}
