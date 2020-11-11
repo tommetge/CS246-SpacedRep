7cbf462ca09b5f0434d75d21d9c12a60da9999a8
tom metge <tom@accident-prone.com>
2020-11-10 19:02:48 -0800

    Wire up the edit popup and add reminder button

--- app/src/main/java/com/cs246/team1/spacedrepetition/EditOrDeletePopUp.java
+++ app/src/main/java/com/cs246/team1/spacedrepetition/EditOrDeletePopUp.java
@@ -2,12 +2,34 @@ package com.cs246.team1.spacedrepetition;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.DialogFragment;
 
 public class EditOrDeletePopUp extends DialogFragment {
+    public interface EditOrDeletePopUpListener {
+        public void onDialogEditClick(DialogFragment dialog);
+        public void onDialogDeleteClick(DialogFragment dialog);
+    }
+
+    EditOrDeletePopUpListener listener;
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            listener = (EditOrDeletePopUpListener) context;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException("Must implement EditOrDeletePopUpListener");
+        }
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
@@ -15,12 +37,12 @@ public class EditOrDeletePopUp extends DialogFragment {
         builder.setMessage(R.string.filler)
                 .setPositiveButton(R.string.edit, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-
+                        listener.onDialogEditClick(EditOrDeletePopUp.this);
                     }
                 })
                 .setNegativeButton(R.string.delete, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-
+                        listener.onDialogDeleteClick(EditOrDeletePopUp.this);
                     }
                 });
         // Create the AlertDialog object and return it
--- app/src/main/java/com/cs246/team1/spacedrepetition/MainActivity.java
+++ app/src/main/java/com/cs246/team1/spacedrepetition/MainActivity.java
@@ -1,10 +1,16 @@
 package com.cs246.team1.spacedrepetition;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.util.Log;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -12,10 +18,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements EditOrDeletePopUp.EditOrDeletePopUpListener {
+
+    private static final String LOGTAG = "MainActivity";
     private static final int RC_SIGN_IN = 123;
     private FirebaseUser user;
+    private List<String> _reminderList = new ArrayList<String>();
+    private ArrayAdapter<String> _reminderAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +42,24 @@ public class MainActivity extends AppCompatActivity {
             TextView text = (TextView) findViewById(R.id.reminderLabel);
             text.setText("Hello, " + user.getDisplayName());
         }
+
+        _reminderAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, _reminderList);
+        ListView reminderView = (ListView) findViewById(R.id.reminderList);
+        reminderView.setAdapter(_reminderAdapter);
+        reminderView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                DialogFragment dialog = new EditOrDeletePopUp();
+                dialog.show(getSupportFragmentManager(), "EditOrDeletePopUpFragment");
+            }
+        });
+
+        _reminderAdapter.clear();
+        _reminderAdapter.add("Test Reminder");
+
     }
-//
+
     public void signOut() {
         AuthUI.getInstance()
                 .signOut(this)
@@ -39,5 +68,23 @@ public class MainActivity extends AppCompatActivity {
                         // ...
                     }
                 });
+    }
+
+    public void showReminder(View view) {
+        Intent intent = new Intent(this, ReminderActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDialogEditClick(DialogFragment dialog) {
+        // User touched the dialog's positive button
+        Log.d(LOGTAG, "Edit pressed");
+        startActivity(new Intent(this, ReminderActivity.class));
+    }
+
+    @Override
+    public void onDialogDeleteClick(DialogFragment dialog) {
+        // User touched the dialog's negative button
+        Log.d(LOGTAG, "Delete pressed");
     }
 }
