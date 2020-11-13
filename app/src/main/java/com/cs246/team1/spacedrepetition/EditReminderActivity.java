a9389632e30d6b9e396ab174482fe2e1aadb0442
sarah <sarah@DESKTOP-OAUA1DU>
2020-11-13 10:45:43 -0700

    added edit reminder layout -sarah

---
+++ app/src/main/java/com/cs246/team1/spacedrepetition/EditReminderActivity.java
@@ -0,0 +1,14 @@
+package com.cs246.team1.spacedrepetition;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_reminder);
+    }
+}
