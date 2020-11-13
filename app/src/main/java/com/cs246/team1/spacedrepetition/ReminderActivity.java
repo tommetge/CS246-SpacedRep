6c592bfe5db2a36682cc06f08b261780c984cd0e
Adrian Ruiz <adrianruizb18@gmail.com>
2020-11-13 17:40:20 -0500

    Added a (basic) Notification. With a button in the reminder Activity to show it. Still need to iterate on this basic notification so that it shows up on a timer, and so that it shows the reminder summary.

--- app/src/main/java/com/cs246/team1/spacedrepetition/ReminderActivity.java
+++ app/src/main/java/com/cs246/team1/spacedrepetition/ReminderActivity.java
@@ -1,14 +1,50 @@
 package com.cs246.team1.spacedrepetition;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ReminderActivity extends AppCompatActivity {
+
+    Button notifyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
+
+        notifyBtn = findViewById(R.id.notif_btn);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("Reminder Notification","Reminder Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        /* This is an OnClick listener for now, until I figure out how to make the notification show up with a timer. */
+        notifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(ReminderActivity.this, "Reminder Notification");
+                builder.setContentTitle("Reminder");
+                /* Need to make the text the reminder summary*/
+                builder.setContentText("Reminder Summary should go here");
+                /* We could add a custom icon as a strech goal */
+                builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(ReminderActivity.this);
+                managerCompat.notify(1, builder.build());
+            }
+        });
     }
+
 }
